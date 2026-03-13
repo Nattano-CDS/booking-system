8fd16ff189cda284853ba71e7ba8d0fa4b89d823
Nattano-CDS <117808089+Nattano-CDS@users.noreply.github.com>
2026-03-13 23:31:11 +0700

    Fetch and display class data from API

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+const API = "https://script.google.com/macros/s/AKfycbzkoUpLUDR03lJnx6Bxunnd3AFJP7CXLpHtiYnA763w1mS019JZE_yutidiISJpSEdp/exec";
 
+fetch(API)
+.then(res=>res.json())
+.then(data=>{
+
+const container = document.getElementById("classList");
+
+data.forEach(cls=>{
+
+const card = document.createElement("div");
+card.className="class-card";
+
+card.innerHTML = `
+
+<div class="class-header">
+<h3>${cls.name}</h3>
+<span>${cls.date} | ${cls.time}</span>
+</div>
+
+<div class="class-detail">
+
+<p>${cls.description}</p>
+
+<p>Price: ${cls.price} THB</p>
+
+<button onclick="book('${cls.id}','${cls.date}','${cls.time}')">
+Book Now
+</button>
+
+</div>
+`;
+
+card.querySelector(".class-header").onclick=()=>{
+
+const detail = card.querySelector(".class-detail");
+
+detail.style.display =
+detail.style.display === "block" ? "none" : "block";
+
+}
+
+container.appendChild(card);
+
+});
+
+});
